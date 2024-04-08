233f15e47f935fcb230efc88166abc17f7c48aa5
Dakarapu Pavitra <pavitra2128@gmail.com>
2024-04-09 00:40:14 +0530

    first commit

---
+++ src/components/MacBookAir9.js
@@ -0,0 +1,254 @@
+import "./MacBookAir9.css";
+
+const MacBookAir = () => {
+  return (
+    <div className="macbook-air-9">
+      <main className="home">
+        <div className="sidebar-wrapper">
+          <div className="sidebar">
+            <div className="iconexlightbox-3-parent">
+              <img
+                className="iconexlightbox-3"
+                loading="lazy"
+                alt=""
+                src="/iconexlightbox-3.svg"
+              />
+              <div className="catalix">Catalix</div>
+            </div>
+            <nav className="sidebar-tab">
+              <div className="sidebar-tab1">
+                <img
+                  className="iconexlighthome"
+                  alt=""
+                  src="/iconexlighthome.svg"
+                />
+                <div className="home1">Home</div>
+              </div>
+              <div className="sidebar-tab2">
+                <img
+                  className="iconexlighttimer"
+                  loading="lazy"
+                  alt=""
+                  src="/iconexlighttimer.svg"
+                />
+                <div className="home2">Activities</div>
+              </div>
+              <div className="sidebar-tab3">
+                <img
+                  className="iconexlightchart"
+                  loading="lazy"
+                  alt=""
+                  src="/iconexlightchart.svg"
+                />
+                <div className="home3">Analytics</div>
+              </div>
+              <div className="sidebar-tab4">
+                <img
+                  className="iconexlightrocket"
+                  loading="lazy"
+                  alt=""
+                  src="/iconexlightrocket.svg"
+                />
+                <div className="home4">Transformation</div>
+              </div>
+              <div className="sidebar-tab5">
+                <img
+                  className="iconexlightlayers"
+                  loading="lazy"
+                  alt=""
+                  src="/iconexlightlayers.svg"
+                />
+                <div className="home5">Library</div>
+              </div>
+            </nav>
+            <img
+              className="sidebar-icon"
+              loading="lazy"
+              alt=""
+              src="/sidebar-icon.svg"
+            />
+            <div className="sidebar-tab-parent">
+              <div className="sidebar-tab6">
+                <img
+                  className="iconexlightsettings"
+                  loading="lazy"
+                  alt=""
+                  src="/iconexlightsettings@2x.png"
+                />
+                <div className="home6">Settings</div>
+              </div>
+              <div className="sidebar-tab7">
+                <img
+                  className="iconexlightlogout"
+                  loading="lazy"
+                  alt=""
+                  src="/iconexlightlogout.svg"
+                />
+                <div className="home7">Logout</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="home-inner">
+          <div className="frame-parent">
+            <div className="text-field-wrapper">
+              <div className="text-field">
+                <div className="text-field1">
+                  <div className="state-layer">
+                    <div className="leading-icon">
+                      <div className="container">
+                        <div className="state-layer1">
+                          <img
+                            className="icon"
+                            loading="lazy"
+                            alt=""
+                            src="/icon.svg"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="content">
+                      <div className="input-text">
+                        <div className="input-text1">
+                          Search by profile, setting, artifact etc...
+                        </div>
+                      </div>
+                      <div className="label-text">
+                        <div className="label-text1">Search</div>
+                      </div>
+                    </div>
+                    <div className="trailing-icon">
+                      <div className="container1">
+                        <div className="state-layer2">
+                          <img
+                            className="icon1"
+                            loading="lazy"
+                            alt=""
+                            src="/icon-1.svg"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="supporting-text">
+                  <div className="supporting-text1">Supporting text</div>
+                </div>
+              </div>
+            </div>
+            <div className="set-up-catalix-for-success-parent">
+              <div className="set-up-catalix-for">
+                Set-up Catalix for Success
+              </div>
+              <div className="get-catalix-up">
+                Get Catalix up to date or start your transformation by following
+                the guide below.
+              </div>
+            </div>
+            <div className="frame-group">
+              <div className="frame-container">
+                <div className="button-wrapper">
+                  <div className="button">
+                    <div className="state-layer3">
+                      <div className="iconex-light-bell">2/6</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="build-your-development">
+                  Build your development value stream map
+                </div>
+              </div>
+              <div className="frame-wrapper">
+                <div className="button-container">
+                  <div className="button1">
+                    <div className="state-layer4">
+                      <div className="label-text2">Start Building</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="frame-div">
+                <div className="ellipse-parent">
+                  <div className="frame-child" />
+                  <div className="frame-item" />
+                  <div className="frame-inner" />
+                  <div className="ellipse-div" />
+                  <div className="frame-child1" />
+                  <div className="frame-child2" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="frame-parent1">
+          <div className="frame-parent2">
+            <div className="component-1-wrapper">
+              <div className="component-1">
+                <img
+                  className="iconexlightbell"
+                  loading="lazy"
+                  alt=""
+                  src="/iconexlightbell@2x.png"
+                />
+                <div className="progress-circles" />
+              </div>
+            </div>
+            <div className="frame-wrapper1">
+              <div className="iconexlightbell-parent">
+                <img
+                  className="iconexlightbell1"
+                  loading="lazy"
+                  alt=""
+                  src="/iconexlightbell-1@2x.png"
+                />
+                <div className="frame-child3" />
+              </div>
+            </div>
+            <div className="frame-wrapper2">
+              <div className="iconexlightsettings-wrapper">
+                <img
+                  className="iconexlightsettings1"
+                  loading="lazy"
+                  alt=""
+                  src="/iconexlightsettings-1@2x.png"
+                />
+              </div>
+            </div>
+            <div className="wrapper-ellipse-49">
+              <img
+                className="wrapper-ellipse-49-child"
+                loading="lazy"
+                alt=""
+                src="/ellipse-49@2x.png"
+              />
+            </div>
+          </div>
+          <div className="video-thumbnail">
+            <div className="get-ready-to-transform-parent">
+              <div className="get-ready-to">Get ready to transform</div>
+              <div className="heres-a-video">
+                Here’s a video to get started. As your transformation
+                progresses, you’ll get fresh tips and insights here.
+              </div>
+            </div>
+            <div className="video-thumbnail1">
+              <div className="component-container">
+                <img className="rene-1-icon" alt="" src="/rene-1@2x.png" />
+                <div className="play-button">
+                  <img className="play-icon" alt="" src="/play.svg" />
+                  <div className="icon-progress-indicator">2:36</div>
+                </div>
+              </div>
+              <div className="progress-indicator">
+                <div className="progress-indicator1" />
+                <div className="highlight" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default MacBookAir;
